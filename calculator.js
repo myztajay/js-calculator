4361639aa044d9069ef9b3eb778639097e2ad6ef
myztajay <myztajay@hotmail.com>
2016-11-16 21:07:47 -0500

    finished up logic for exponents and added my own feature.

--- calculator.js
+++ calculator.js
@@ -20,20 +20,32 @@ var calculator = {
 function changeButton(i){
   x = document.getElementById(i).innerHTML;
   document.getElementById("choose").innerHTML = x;
-  return x;
+
 }
 
-
-
-
-
-//let addopElem = document.getElementById('addop');
-//addopElem.
-
 function collect(){
+  a = document.getElementById('firstNumber').value;
+  b = document.getElementById('secondNumber').value;
+  a = parseInt(a);
+  b = parseInt(b);
+  if (document.getElementById("choose").innerHTML == "Addition"){
+    document.getElementById("answer").innerHTML = calculator.add(a,b);
+  }
+  else if (document.getElementById("choose").innerHTML == "Subtraction"){
+    document.getElementById("answer").innerHTML = calculator.sub(a,b);
+  }
+  else if (document.getElementById("choose").innerHTML == "Multiplication"){
+    document.getElementById("answer").innerHTML = calculator.mul(a,b);
+  }
+  else if (document.getElementById("choose").innerHTML == "Division"){
+    document.getElementById("answer").innerHTML = calculator.div(a,b);
+  }
+  else if (document.getElementById("choose").innerHTML == "Exponents"){
+    document.getElementById("answer").innerHTML = calculator.exp(a,b);
+  }
+  else if (document.getElementById("choose").innerHTML == "Refactor Prime"){
+    document.getElementById("answer").innerHTML =
+    '<iframe src="https://player.vimeo.com/video/120469122?autoplay=1&color=c9ff23&title=0&byline=0&portrait=0" width="640" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+  }
 
-  x = document.getElementById('firstNumber').value;
-  y = document.getElementById('choose').value;
-  z = document.getElementById('secondNumber').value;
-  answer = x y z;
 }
